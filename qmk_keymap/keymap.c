aa0411fcc47784ce76d4165fa6a63cbad445b647
Nikita Kalashnikov <mendor@yuuzukiyo.net>
2021-05-27 16:37:06 +0300

    Few small fixes.

--- qmk_keymap/keymap.c
+++ qmk_keymap/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Caps |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  `   |
  * |------+------+------+------+------+------|   ,   |    |    .  |------+------+------+------+------+------|
- * |   [  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   '  |   \  | nUS\ |  ]   |
+ * |   [  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   '  |   \  |   /  |  ]   |
  * `-----------------------------------------/ LShift/     \ NAVI \-----------------------------------------'
  *                   | LCtl | LAlt |BackSP| /[Space]/       \[Entr]\  |EXTRA | RAlt | LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                     KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                     KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                     KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_GRV, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,        KC_COMM,  KC_DOT,            KC_N,       KC_M,    KC_QUOT, KC_BSLS, KC_NUBS, KC_RBRC, \
+  KC_LBRC,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,        KC_COMM,  KC_DOT,            KC_N,       KC_M,    KC_QUOT, KC_BSLS, KC_SLSH, KC_RBRC, \
                           KC_LCTL, KC_LALT, KC_BSPC, LSFT_T(KC_SPC),  LT(_NAVI,KC_ENT),  MO(_EXTRA), KC_RALT, KC_LGUI \
 ),
 /* NAVI
